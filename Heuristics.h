25c3d492e9a5f44be21071c0e24642d7d6626c61
Saul Ramirez <26sramirez6@gmail.com>
2020-02-14 12:07:09 -0600

    a

--- Heuristics.h
+++ Heuristics.h
@@ -11,20 +11,16 @@ struct Manhattan {
 	}
 };
 
-//template<typename NodeT>
-//struct Diagonal {
-//	FORCEINLINE int16_t calc(NodeT const * current, NodeT const * next) const {
-//		return std::abs(current->x - next->x) +
-//			std::abs(current->y - next->y) +
-//			std::abs(current->z - next->z);
-//	}
-//};
-//
-//function heuristic(node) =
-//dx = abs(node.x - goal.x)
-//dy = abs(node.y - goal.y)
-//return D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
+template<typename NodeT>
+struct Chebyshev {
+	FORCEINLINE int16_t calc(NodeT const * current, NodeT const * next) const { 
+		const uint16_t _dx = std::abs(current->x - next->x);
+		const uint16_t _dy = std::abs(current->y - next->y);
+		return (_dx + _dy) + -(_dx > _dy ? _dy : _dx);
+	}
+};
+
 
 template<typename T> struct IsHeuristic : std::integral_constant<bool, false> {};
 template<typename NodeT> struct IsHeuristic<Manhattan<NodeT>> : std::integral_constant<bool, true> {};
-//template<typename NodeT> struct IsHeuristic<Diagonal<NodeT>> : std::integral_constant<bool, true> {};
+template<typename NodeT> struct IsHeuristic<Chebyshev<NodeT>> : std::integral_constant<bool, true> {};
